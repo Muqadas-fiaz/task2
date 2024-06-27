5f974b4914ef7088b400e3eae9586455de3559e1
muqadas <muqadas@localhost.localdomain>
2024-06-27 06:22:48 +0100

    Add system info script

--- system_info.sh
+++ system_info.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Define your name
-your_name="Your Name"
+your_name="Muquadas"
 
 # Get CPU details
-cpu_details=$(lscpu | grep "Model name" | awk -F: "{print \$2}" | sed "s/^ *//g")
+cpu_details=$(lscpu | grep 'Model name' | awk -F: '{print $2}' | sed 's/^ *//g')
 
 # Get RAM details
-ram_details=$(free -h | grep "Mem:" | awk "{print \$2}")
+ram_details=$(free -h | grep 'Mem:' | awk '{print $2}')
 
 # Get HDD details
-hdd_details=$(df -h --total | grep "total" | awk "{print \$2}")
+hdd_details=$(df -h --total | grep 'total' | awk '{print $2}')
 
 # Output the details in the desired format
 echo "name:$your_name"
 echo "cpu:$cpu_details"
 echo "ram:$ram_details"
 echo "hdd:$hdd_details"
-
